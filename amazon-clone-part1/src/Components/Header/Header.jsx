694bb973e46abb91793b5eb8bcd1fa5cf243773b
Yohannes Geberhanna <yohat7@gmail.com>
2024-11-08 20:20:20 -0500

    Create header part

---
+++ amazon-clone-part1/src/Components/Header/Header.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import classes from "./Header.module.css";
+import { SlLocationPin } from "react-icons/sl";
+import { BsSearch } from "react-icons/bs";
+import { BiCart } from "react-icons/bi";
+
+const Header = () => {
+
+  return (
+    <>
+    <section>
+        <div className={classes.header__container}>
+          {/* logo section */}
+          <div className={classes.logo__container}>
+            <a href="#">
+              <img
+                src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                alt="amazon logo" />       
+             </a>
+            
+            <div className={classes.delivery}>
+              <span>
+                <SlLocationPin />
+              </span>
+              <div>
+                <p>Deliver to</p>
+                <span>Ethiopia</span>
+              </div>
+            </div>
+          </div>
+          {/* search section */}
+          <div className={classes.search}>
+            <select name="" id="">
+              <option value="">All</option>
+            </select>
+            <input type="text" name="" id="" placeholder="Search Amazon" />
+            <BsSearch size={25} />
+          </div>
+          {/* right side  section */}
+          <div className={classes.order__container}>
+            <a href="" className={classes.language}>
+              <img
+                src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/1024px-Flag_of_the_United_States.svg.png"
+                alt=""
+              />
+              <select name="" id="">
+                <option value="">EN</option>
+              </select>
+           </a>
+            {/* sign in  */}
+            <a href="">
+               <p>Sign In</p>
+               <span>Account & Lists</span>
+            </a>
+               {/* orders returns */}
+            <a href="">
+              <p>Returns</p>
+              <span>& Orders</span>
+            </a>
+            {/* cart */}
+            <a href="" className={classes.cart}>
+              <BiCart size={35} />
+              <span>0</span>
+            </a>
+           </div>
+        </div>
+    </section>
+    </>
+  );
+};
+
+export default Header;
